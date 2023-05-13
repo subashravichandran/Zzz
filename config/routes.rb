6ce324a38896f2f3a326368647414de64b580b81
Subash R <waytosubash@gmail.com>
2023-05-13 17:56:49 +0530

    Use Login page as landing page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "dashboard#index"
+  devise_scope :user do
+    authenticated :user do
+      root "dashboard#index", as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
